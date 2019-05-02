7acff1221c1379fa2056b00b4e2f1305fd4b3e90
Alexey Evseev @stalk <myhappydo@gmail.com>
2019-05-02 15:12:22 +0300

    Use pre_save signal for creating fields, not post_save

--- audit_log/middleware.py
+++ audit_log/middleware.py
@@ -6,14 +6,6 @@ from audit_log import registration, settings
 from audit_log.models import fields
 from audit_log.models.managers import AuditLogManager
 
-def _disable_audit_log_managers(instance):
-    for attr in dir(instance):
-        try:
-            if isinstance(getattr(instance, attr), AuditLogManager):
-                getattr(instance, attr).disable_tracking()
-        except AttributeError:
-            pass
-
 
 def _enable_audit_log_managers(instance):
     for attr in dir(instance):
@@ -35,25 +27,36 @@ class UserLoggingMiddleware(MiddlewareMixin):
                 user = None
             session = request.session.session_key
             update_pre_save_info = curry(self._update_pre_save_info, user, session)
-            update_post_save_info = curry(self._update_post_save_info, user, session)
             signals.pre_save.connect(update_pre_save_info,  dispatch_uid = (self.__class__, request,), weak = False)
-            signals.post_save.connect(update_post_save_info,  dispatch_uid = (self.__class__, request,), weak = False)
 
     def process_response(self, request, response):
         if settings.DISABLE_AUDIT_LOG:
             return
         signals.pre_save.disconnect(dispatch_uid =  (self.__class__, request,))
-        signals.post_save.disconnect(dispatch_uid =  (self.__class__, request,))
         return response
 
     def process_exception(self, request, exception):
         if settings.DISABLE_AUDIT_LOG:
             return None
         signals.pre_save.disconnect(dispatch_uid =  (self.__class__, request,))
-        signals.post_save.disconnect(dispatch_uid =  (self.__class__, request,))
         return None
 
     def _update_pre_save_info(self, user, session, sender, instance, **kwargs):
+        # creating fields
+        if not instance.pk:
+            registry = registration.FieldRegistry(fields.CreatingUserField)
+            if sender in registry:
+                for field in registry.get_fields(sender):
+                    if not getattr(instance, field.name, None):
+                        setattr(instance, field.name, user)
+
+            registry = registration.FieldRegistry(fields.CreatingSessionKeyField)
+            if sender in registry:
+                for field in registry.get_fields(sender):
+                    if not getattr(instance, field.name, None):
+                        setattr(instance, field.name, session)
+
+        # modifying fields
         registry = registration.FieldRegistry(fields.LastUserField)
         if sender in registry:
             for field in registry.get_fields(sender):
@@ -63,31 +66,6 @@ class UserLoggingMiddleware(MiddlewareMixin):
         if sender in registry:
             for field in registry.get_fields(sender):
                 setattr(instance, field.name, session)
-
-
-    def _update_post_save_info(self, user, session, sender, instance, created, **kwargs ):
-        if created:
-            registry = registration.FieldRegistry(fields.CreatingUserField)
-            if sender in registry:
-                for field in registry.get_fields(sender):
-                    if not getattr(instance, field.name, None):
-                        setattr(instance, field.name, user)
-                        _disable_audit_log_managers(instance)
-                        instance.save()
-                        _enable_audit_log_managers(instance)
-
-
-            registry = registration.FieldRegistry(fields.CreatingSessionKeyField)
-            if sender in registry:
-                for field in registry.get_fields(sender):
-                    if not getattr(instance, field.name, None):
-                        setattr(instance, field.name, session)
-                        _disable_audit_log_managers(instance)
-                        instance.save()
-                        _enable_audit_log_managers(instance)
-
-
-
 
 
 class JWTAuthMiddleware(MiddlewareMixin):
