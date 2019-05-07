a6a88d492295c2efe3e2a0f22d03e1882f8fae60
Alexey Evseev @stalk <myhappydo@gmail.com>
2019-05-08 01:37:26 +0300

    Respect nested context managers

--- audit_log/context_managers.py
+++ audit_log/context_managers.py
@@ -1,39 +1,50 @@
 
 def _is_creating_user_tracking_disabled(model_or_instance):
-    return getattr(model_or_instance, '__is_creating_user_tracking_disabled', False)
+    return getattr(model_or_instance, '__creating_user_tracking_disabled_cnt', 0) == 0
 
 
 def _is_last_user_tracking_disabled(model_or_instance):
-    return getattr(model_or_instance, '__is_last_user_tracking_disabled', False)
+    return getattr(model_or_instance, '__last_user_tracking_disabled_cnt', 0) == 0
+
+
+def _incr_count(model_or_instance, field_name):
+    count = getattr(model_or_instance, field_name, 0)
+    setattr(model_or_instance, field_name, count + 1)
+
+
+def _decr_count(model_or_instance, field_name):
+    count = getattr(model_or_instance, field_name, 0)
+    count -= 1 if count > 0 else 0
+    setattr(model_or_instance, field_name, count)
 
 
 def disable_creating_user_tracking(*model_or_instances):
     try:
         for model_or_instance in model_or_instances:
-            setattr(model_or_instance, '__is_creating_user_tracking_disabled', True)
+            _incr_count(model_or_instance, '__creating_user_tracking_disabled_cnt')
         yield
     finally:
         for model_or_instance in model_or_instances:
-            setattr(model_or_instance, '__is_creating_user_tracking_disabled', False)
+            _decr_count(model_or_instance, '__creating_user_tracking_disabled_cnt')
 
 
 def disable_last_user_tracking(*model_or_instances):
     try:
         for model_or_instance in model_or_instances:
-            setattr(model_or_instance, '__is_last_user_tracking_disabled', True)
+            _incr_count(model_or_instance, '__last_user_tracking_disabled_cnt')
         yield
     finally:
         for model_or_instance in model_or_instances:
-            setattr(model_or_instance, '__is_last_user_tracking_disabled', False)
+            _decr_count(model_or_instance, '__last_user_tracking_disabled_cnt')
 
 
 def disable_user_tracking(*model_or_instances):
     try:
         for model_or_instance in model_or_instances:
-            setattr(model_or_instance, '__is_last_user_tracking_disabled', True)
-            setattr(model_or_instance, '__is_creating_user_tracking_disabled', True)
+            _incr_count(model_or_instance, '__last_user_tracking_disabled_cnt')
+            _incr_count(model_or_instance, '__creating_user_tracking_disabled_cnt')
         yield
     finally:
         for model_or_instance in model_or_instances:
-            setattr(model_or_instance, '__is_last_user_tracking_disabled', False)
-            setattr(model_or_instance, '__is_creating_user_tracking_disabled', False)
+            _decr_count(model_or_instance, '__creating_user_tracking_disabled_cnt')
+            _decr_count(model_or_instance, '__last_user_tracking_disabled_cnt')
