2fa8c09893538083af27dd7e9041652f194a800f
Alexey Evseev @stalk <myhappydo@gmail.com>
2019-05-08 00:14:14 +0300

    Disable audit for many models in one context manager

--- audit_log/context_managers.py
+++ audit_log/context_managers.py
@@ -7,27 +7,33 @@ def _is_last_user_tracking_disabled(model_or_instance):
     return getattr(model_or_instance, '__is_last_user_tracking_disabled', False)
 
 
-def disable_creating_user_tracking(model_or_instance):
+def disable_creating_user_tracking(*model_or_instances):
     try:
-        setattr(model_or_instance, '__is_creating_user_tracking_disabled', True)
+        for model_or_instance in model_or_instances:
+            setattr(model_or_instance, '__is_creating_user_tracking_disabled', True)
         yield
     finally:
-        setattr(model_or_instance, '__is_creating_user_tracking_disabled', False)
+        for model_or_instance in model_or_instances:
+            setattr(model_or_instance, '__is_creating_user_tracking_disabled', False)
 
 
-def disable_last_user_tracking(model_or_instance):
+def disable_last_user_tracking(*model_or_instances):
     try:
-        setattr(model_or_instance, '__is_last_user_tracking_disabled', True)
+        for model_or_instance in model_or_instances:
+            setattr(model_or_instance, '__is_last_user_tracking_disabled', True)
         yield
     finally:
-        setattr(model_or_instance, '__is_last_user_tracking_disabled', False)
+        for model_or_instance in model_or_instances:
+            setattr(model_or_instance, '__is_last_user_tracking_disabled', False)
 
 
-def disable_user_tracking(model_or_instance):
+def disable_user_tracking(*model_or_instances):
     try:
-        setattr(model_or_instance, '__is_last_user_tracking_disabled', True)
-        setattr(model_or_instance, '__is_creating_user_tracking_disabled', True)
+        for model_or_instance in model_or_instances:
+            setattr(model_or_instance, '__is_last_user_tracking_disabled', True)
+            setattr(model_or_instance, '__is_creating_user_tracking_disabled', True)
         yield
     finally:
-        setattr(model_or_instance, '__is_last_user_tracking_disabled', False)
-        setattr(model_or_instance, '__is_creating_user_tracking_disabled', False)
+        for model_or_instance in model_or_instances:
+            setattr(model_or_instance, '__is_last_user_tracking_disabled', False)
+            setattr(model_or_instance, '__is_creating_user_tracking_disabled', False)
