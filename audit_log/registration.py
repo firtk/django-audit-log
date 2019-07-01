46f01f5b1a85076e2680e6c3b07ea54d89118c9c
Alexey Evseev @stalk <myhappydo@gmail.com>
2019-07-02 02:04:41 +0300

    Allow to inherit audit fields
    Currently, fields inherited from audit_log fields doesn't work:
    
    ```python
    from audit_log.models.fields import CreatingUserField
    
    class MyCustomCreatingUserField(CreatingUserField):
        pass
    
    class MyModel(models.Model):
        created_by = MyCustomCreatingUserField()
    ```
    
    If MyModel was created in view - created_by will not be filled.
    
    This patch tends to fix it: inherited fields will still work.
    It will allow to customize field if needed.

--- audit_log/registration.py
+++ audit_log/registration.py
@@ -1,17 +1,27 @@
+import inspect
+
+
 class FieldRegistry(object):
     _registry = {}
-    
-    def __init__(self, fieldcls):
-        self._fieldcls = fieldcls
+    audit_field_module = 'audit_log.models.fields'
 
-    
+    def __init__(self, fieldcls):
+        # Save first audit base class, to allow audit_log field inheritance
+        first_audit_base_class = None
+        if fieldcls.__module__ != self.audit_field_module:
+            for base_class in inspect.getmro(fieldcls):
+                if base_class.__module__ == self.audit_field_module:
+                    first_audit_base_class = base_class
+                    break
+        self._fieldcls = first_audit_base_class or fieldcls
+
     def add_field(self, model, field):
         reg = self.__class__._registry.setdefault(self._fieldcls, {}).setdefault(model, [])
         reg.append(field)
-    
+
     def get_fields(self, model):
         return self.__class__._registry.setdefault(self._fieldcls, {}).get(model, [])
-    
+
     def __contains__(self, model):
         return model in self.__class__._registry.setdefault(self._fieldcls, {})
 
